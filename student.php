c3be9f7aaf3fc48eabdb5935df3c69282d598e18
dprieler <dprieler@gmail.com>
2017-04-11 08:36:45 +0200

    lösch routine für bildungsstandard

--- student.php
+++ student.php
@@ -39,78 +39,36 @@ $navnode->make_active();// build tab navigation & print header
 
 echo $OUTPUT->header();
 
-/*
-if (optional_param('delete_exaport', null, PARAM_RAW)) {
-	$action = 'delete_exaport';
-} elseif (optional_param('delete_exastud', null, PARAM_RAW)) {
-	$action = 'delete_exastud';
-} elseif (optional_param('delete_exacomp', null, PARAM_RAW)) {
-	$action = 'delete_exacomp';
-} else {
-	$action = '';
-}
+$bildungsstandard = block_exadelete\check_block_available('exastud') ? \block_exastud\api::get_bildungsstandard_erreicht($USER->id) : null;
 
-if ($action) {
-	require_sesskey();
-
-	switch ($action) {
-		case 'delete_exacomp':
-			if (!block_exadelete\check_block_available('exacomp')) {
-				throw new moodle_exception('action not available');
-			}
-
-			$userids = clean_param_array(explode(',', required_param('userids', PARAM_SEQUENCE)), PARAM_INT);
-
-			foreach ($userids as $userid) {
-				block_exacomp\api::delete_user_data($userid);
-			}
-
-			break;
-		case 'delete_exaport':
-			if (!block_exadelete\check_block_available('exaport')) {
-				throw new moodle_exception('action not available');
-			}
-
-			$userids = clean_param_array(explode(',', required_param('userids', PARAM_SEQUENCE)), PARAM_INT);
-
-			foreach ($userids as $userid) {
-				block_exaport\api::delete_user_data($userid);
-			}
-
-			break;
-		case('delete_exastud'):
-			if (!block_exadelete\check_block_available('exastud')) {
-				throw new moodle_exception('action not available');
-			}
-
-			$userids = clean_param_array(explode(',', required_param('userids', PARAM_SEQUENCE)), PARAM_INT);
-
-			foreach ($userids as $userid) {
-				block_exastud\api::delete_user_data($userid);
-			}
-
-			break;
-		default:
-			print_error('wrong action: '.$action);
+if (!$bildungsstandard) {
+	?>
+		Du hast noch keinen Bildungsstandard erreicht
+	<?php
+} elseif (optional_param('action', null, PARAM_TEXT) == 'delete') {
+	if (block_exadelete\check_block_available('exacomp')) {
+		block_exacomp\api::delete_student_assessment_data_up_until($USER->id, $bildungsstandard->bildungsstandard_erreicht_time);
 	}
 
-	notice(\block_exadelete\trans(['de:Ausgewählte Benutzerdaten wurden entfernt!']), new moodle_url('/blocks/exadelete/deleteexabis.php'));
-}
-*/
+	echo 'Deine Daten wurden gelöscht.';
+} else {
+	$dateformat = get_string('strftimedate');
 
-//CONTENT-REGION
-?>
-	Du hast den Bildungsstandard 5-6 erreicht und kannst hier die Daten in den Kompetenzrastern unwiderruflich löschen:
-	<br/>
-	<ul>
-		<li>Alle Selbsteinschätzungen</li>
-		<li>Alle Lehrerbewertungen</li>
-	</ul>
-	<br/>
-	<input type="button"
-		   value="Kompetenzraster des Bildungsstandard 5-6 jetzt löschen"
-		   onclick="if (confirm('Wirklich löschen?')) alert('TODO: Löschroutine hier einbinden');"
-	/>
-<?php
+	?>
+		Du hast den Bildungsstandard <?=$bildungsstandard->bildungsstandard_erreicht?> am <?=userdate($bildungsstandard->bildungsstandard_erreicht_time, $dateformat)?> erreicht und kannst hier die Daten in den Kompetenzrastern unwiderruflich löschen:
+		<br/>
+		<ul>
+			<li>Alle Selbsteinschätzungen</li>
+			<li>Alle Lehrerbewertungen</li>
+		</ul>
+		<br/>
+		<form method="post">
+			<input type="hidden" name="action" value="delete"/>
+			<input type="submit"
+				   value="Kompetenzraster des Bildungsstandard <?=$bildungsstandard->bildungsstandard_erreicht?> jetzt löschen"
+			/>
+		</form>
+	<?php
+}
 
 echo $OUTPUT->footer();
